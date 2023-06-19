ed0895fc8c1f4a7aefe1eade3dafac5b139dd015
CHRISTIANAGUIRRE16 <ontanedamarcelo74@gmail.com>
2023-06-19 15:28:44 -0500

    cambios

--- lib/pages/qr_code.dart
+++ lib/pages/qr_code.dart
@@ -1,14 +1,15 @@
 
 import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 class PageCode extends StatefulWidget {
-  const PageCode({super.key});
-
   @override
-  State<PageCode> createState() => _PageCodeState();
+   _PageCodeState createState() => _PageCodeState();
 }
 
 class _PageCodeState extends State<PageCode> {
+  final qrKey = GlobalKey();
+  Barcode? scannedBarcode;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,6 +17,42 @@ class _PageCodeState extends State<PageCode> {
         title: Text('Codigo QR'),
         backgroundColor: Colors.orange,
       ),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 4,
+            child: QRView(
+              key: qrKey,
+              onQRViewCreated: _onQRViewCreated,
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Center(
+              child: Text(
+                scannedBarcode != null
+                    ? 'QR Code: ${scannedBarcode!.code}'
+                    : 'No QR code scanned',
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
+
+  void _onQRViewCreated(QRViewController controller) {
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        scannedBarcode = scanData;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    qrKey.currentState?.dispose();
+    super.dispose();
+  }
 }
+    
